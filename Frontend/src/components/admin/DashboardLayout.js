81759d99bef650291d427ce64ce8b17f78453f40
FahdJerbi <fahd.jerbi@fshst.u-tunis.com>
2022-09-18 01:02:47 +0100

    add "Admin Navbar"

--- Frontend/src/components/admin/DashboardLayout.js
+++ Frontend/src/components/admin/DashboardLayout.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import DashboardSideBar from "./DashboardSideBar";
+import AdminNavbar from "./navbar/AdminNavbar";
 
 function DashboardLayout() {
   return (
     <div>
+      <AdminNavbar />
       <DashboardSideBar />
       <div
         style={{
           border: "0.5px grey solid",
-          marginTop: "70px",
+          // marginTop: "70px",
           width: "82%",
           marginLeft: "240px",
         }}
       >
-        {" "}
         <Outlet />
       </div>
     </div>
