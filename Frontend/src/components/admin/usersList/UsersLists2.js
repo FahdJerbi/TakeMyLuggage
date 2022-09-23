ec149087fe3d32101955bbd5064bf7c8ffa0f5ec
FahdJerbi <fahd.jerbi@fshst.u-tunis.com>
2022-09-23 16:42:13 +0100

    add "delete user with Redux"

--- Frontend/src/components/admin/usersList/UsersLists2.js
+++ Frontend/src/components/admin/usersList/UsersLists2.js
@@ -1,101 +1,138 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import DeleteOutline from "@mui/icons-material/DeleteOutline";
+import Button from "@mui/material/Button";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllUsers } from "../../../redux/adminSlice";
+import { getAllUsers, deleteUser } from "../../../redux/adminSlice";
 
-const columns = [
-  { field: "_id", headerName: "ID", width: 225 },
-  {
-    field: "email",
-    headerName: "Email",
-    type: "string",
-    width: 200,
-  },
-  {
-    field: `firstName`,
-    headerName: "First Name",
-    // type: "string",
-    width: 130,
-  },
-  {
-    field: `lastName`,
-    headerName: "Last Name",
-    // type: "string",
-    width: 130,
-  },
-  {
-    field: "isAdmin",
-    headerName: "Admin",
-    type: "string",
-    width: 150,
-  },
-  // {
-  //   field: "status",
-  //   headerName: "Status",
-  //   type: "string",
-  //   width: 200,
-  // },
-  {
-    field: "actions",
-    headerName: "Actions",
-    description: "This column has a value getter and is not sortable.",
-    width: 196,
-    renderCell: (params) => {
-      return (
-        <>
-          <DeleteOutline
-            className="userListDelete"
-            // onClick={() => handleDelete(params.row.id)}
-          />
-        </>
-      );
-    },
-  },
-];
-
-// const rows = [
+// const columns = [
+//   { field: "_id", headerName: "ID", width: 225 },
 //   {
-//     id: 1,
-//     user: "Jon Snow",
-//     // status: "Available",
-//     email: "Jon@gmail.com",
-//     actions: "delete/update",
+//     field: "email",
+//     headerName: "Email",
+//     type: "string",
+//     width: 200,
 //   },
 //   {
-//     id: 2,
-//     user: "Cersei Lannister",
-//     // status: "Unavailable",
-//     email: "cersei@gmail.com",
-//     actions: "delete/update",
+//     field: `firstName`,
+//     headerName: "First Name",
+//     // type: "string",
+//     width: 130,
 //   },
 //   {
-//     id: 3,
-//     user: "Cersei Lannister",
-//     // status: "Unavailable",
-//     email: "cersei@gmail.com",
-//     actions: "delete/update",
+//     field: `lastName`,
+//     headerName: "Last Name",
+//     // type: "string",
+//     width: 130,
+//   },
+//   {
+//     field: "isAdmin",
+//     headerName: "Admin",
+//     type: "string",
+//     width: 150,
+//   },
+//   // {
+//   //   field: "status",
+//   //   headerName: "Status",
+//   //   type: "string",
+//   //   width: 200,
+//   // },
+//   {
+//     field: "actions",
+//     headerName: "Actions",
+//     description: "This column has a value getter and is not sortable.",
+//     width: 196,
+//     renderCell: (params) => {
+//       return (
+//         <>
+//           <Button>
+//             <DeleteOutline
+//               className="userListDelete"
+//               // onClick={() => console.log(params.row._id)}
+//               // onClick={() => dispatch(deleteUser(params.row._id))}
+//             />
+//           </Button>
+//         </>
+//       );
+//     },
 //   },
 // ];
 
 export default function UsersList() {
-  const [usersInfo, setUsersInfo] = React.useState([]);
+  // const [usersInfo, setUsersInfo] = React.useState(users);
 
   // Redux prep
-  const { users, loading } = useSelector((state) => state.admin);
+  // const { users, loading } = useSelector((state) => state.admin);
+  const { users } = useSelector((state) => state.admin);
   const dispatch = useDispatch();
 
   // console.log("usersInfo:", usersInfo);
 
   // -------------------------------    Order is working  ------------------
 
+  const handleDelete = (id) => {
+    dispatch(deleteUser(id));
+  };
+
+  // console.log(usersInfo);
+
   // React.useEffect(() => {
-  //   axios
-  //     .get("/api/getOrders")
-  //     .then((res) => console.log(res.data.data))
-  //     .catch((err) => console.log(err));
-  // }, []);
+  //   console.log("ok");
+  // }, [usersInfo]);
+
+  const columns = [
+    { field: "_id", headerName: "ID", width: 225 },
+    {
+      field: "email",
+      headerName: "Email",
+      type: "string",
+      width: 200,
+    },
+    {
+      field: `firstName`,
+      headerName: "First Name",
+      // type: "string",
+      width: 130,
+    },
+    {
+      field: `lastName`,
+      headerName: "Last Name",
+      // type: "string",
+      width: 130,
+    },
+    {
+      field: "isAdmin",
+      headerName: "Admin",
+      type: "string",
+      width: 150,
+    },
+    // {
+    //   field: "status",
+    //   headerName: "Status",
+    //   type: "string",
+    //   width: 200,
+    // },
+    {
+      field: "actions",
+      headerName: "Actions",
+      description: "This column has a value getter and is not sortable.",
+      width: 196,
+      renderCell: (params) => {
+        return (
+          <>
+            <Button>
+              <DeleteOutline
+                sx={{ color: "red" }}
+                className="userListDelete"
+                onClick={() => handleDelete(params.row._id)}
+              />
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
 
   React.useEffect(() => {
     dispatch(getAllUsers());
@@ -113,6 +150,7 @@ export default function UsersList() {
         pageSize={10}
         rowsPerPageOptions={[10]}
         checkboxSelection
+        disableSelectionOnClick
       />
     </div>
   );
--- Frontend/src/redux/adminSlice.js
+++ Frontend/src/redux/adminSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+// **************************************       Thunks       **********************
 // create the getAllOrders thunk
 export const getAllOrders = createAsyncThunk("admin/getOrders", async () => {
   const response = await axios.get("/api/admin/getOrders");
@@ -19,12 +20,19 @@ export const getAllDrivers = createAsyncThunk("admin/getDrivers", async () => {
   return response.data.data;
 });
 
-// export const getAllOrders = createAsyncThunk("admin/getOrders", async () => {
-//   const response = await axios.get(
-//     "https://jsonplaceholder.typicode.com/users"
-//   );
-//   return response.data;
-// });
+// ------------------------------------   delete user thunk   ---------------------
+export const deleteUser = createAsyncThunk(
+  "admin/deleteUsers",
+  async (id, { rejectWithValue }) => {
+    try {
+      const response = await axios.delete(`/api/admin/deleteUser/${id}`);
+      // console.log("thunk is working !");
+      return response.data.data;
+    } catch (error) {
+      return rejectWithValue(error.response);
+    }
+  }
+);
 
 const initialState = {
   orders: [],
@@ -49,7 +57,7 @@ export const adminSlice = createSlice({
     [getAllOrders.rejected]: (state, action) => {
       state.loading = false;
     },
-
+    // ------------------------------------------------------
     // getAllUsers thunk
     [getAllUsers.pending]: (state, action) => {
       state.loading = true;
@@ -61,7 +69,7 @@ export const adminSlice = createSlice({
     [getAllUsers.rejected]: (state, action) => {
       state.loading = false;
     },
-
+    // ------------------------------------------------------
     // getAllDrivers thunk
     [getAllDrivers.pending]: (state, action) => {
       state.loading = true;
@@ -71,6 +79,21 @@ export const adminSlice = createSlice({
       state.drivers = action.payload;
     },
     [getAllDrivers.rejected]: (state, action) => {
+      state.loading = false;
+    },
+    // ------------------------------------------------------
+    // deleteUser thunk
+    [deleteUser.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [deleteUser.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.users = state.users.filter(
+        (user) => user._id !== action.payload._id
+      );
+      // console.log("action payload:", action);
+    },
+    [deleteUser.rejected]: (state, action) => {
       state.loading = false;
     },
   },
