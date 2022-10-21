b09ee3a8c2bc4e076a8226bb74cc53bc57da7b4f
FahdJerbi <fahd.jerbi@fshst.u-tunis.com>
2022-10-21 13:37:47 +0100

    add "Landing Page"

--- Frontend/src/components/views/Layout.js
+++ Frontend/src/components/views/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../layout/Navbar";
 import { Outlet } from "react-router-dom";
+import HomePage from "../landing-page/HomePage";
 
 const Layout = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
+      <HomePage />
       <Outlet />
     </div>
   );
